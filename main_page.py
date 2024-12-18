d67263ac5c0d4f1ccea566ae24bd96dc5963354d
xiaozhisky1 <2425675699@qq.com>
2024-12-18 11:24:24 +0800

    debug_mainpage

--- main_page.py
+++ main_page.py
@@ -1,10 +1,5 @@
 import streamlit as st
-import random
-import duckdb
-import numpy as np
-import pandas as pd
-import plotly.express as px
-import plotly.graph_objects as go
+
 
 st.set_page_config(
     page_title="Hello",
